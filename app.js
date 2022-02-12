18cf8a86ea44dbcabfbd9c9cfded839be80a91ab
plwie <58283153+plwie@users.noreply.github.com>
2022-02-12 18:26:04 +0700

    add routing

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const chalk  =require('chalk');
+const chalk = require('chalk');
+const fees = require("./data/fees.json");
+const priceRouter = express.Router()
 
 const app = express();
 const Port = process.env.Port;
@@ -13,10 +15,16 @@ app.use(express.static(path.join(__dirname, "/public/")));
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+priceRouter.route("/").get((req, res) => {
+    res.render("price",
+        fees);
+})
+
+app.use("/prices", priceRouter)
 app.get("/", (req, res) => {
-    res.render('index', {username: "inwza"});
+    res.render('index', { username: "inwza" });
 });
- 
-app.listen(Port, ()=> {
+
+app.listen(Port, () => {
     debug("listenning on Port " + chalk.green(Port));
 });
