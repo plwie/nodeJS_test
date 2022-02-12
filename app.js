6472a0946284db8fb6fac52b614bfd9415b3ae81
plwie <58283153+plwie@users.noreply.github.com>
2022-02-12 16:05:08 +0700

    add index

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
-// app.use(express.static(path.join(__dirname, "/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
     res.send('hello world');
-})
+});
+
+app.listen(port, ()=> {
+    console.log("listenning on port %d", port);
+});
