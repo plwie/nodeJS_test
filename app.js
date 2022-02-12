df9a02d4b343c91f6b1ee2db92c88be0d7d8bd0b
plwie <58283153+plwie@users.noreply.github.com>
2022-02-12 16:56:33 +0700

    add nodemon

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const path = require('path');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const chalk  =require('chalk');
 
 const app = express();
-const port = 3000;
+const Port = process.env.Port;
 
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
-    res.send('hello world');
+    res.send('hello world 3');
 });
  
-app.listen(port, ()=> {
-    console.log("listenning on port %d", port);
+app.listen(Port, ()=> {
+    debug("listenning on Port " + chalk.green(Port));
 });
